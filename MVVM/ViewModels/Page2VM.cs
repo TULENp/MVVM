e4699e26d69f8ce1d272f768387ff0452c384df7
Eugene <zh_kononenko3@mail.ru>
2022-02-10 21:08:23 +0300

    Add project files.

---
+++ MVVM/ViewModels/Page2VM.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Windows.Input;
+
+namespace MVVM.ViewModels
+{
+    public class Users
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("first_name")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("last_name")]
+        public string LastName { get; set; }
+
+        [JsonProperty("avatar")]
+        public Uri Avatar { get; set; }
+    }
+    public class MyResponce
+    {
+        public List<Users> Responce { get; set; }
+    }
+
+    class Page2VM : BindableObject
+    {
+        private ObservableCollection<Users> data;
+        private HttpClient client;
+        private object source;
+        public object Source
+        {
+            get => source;
+            set
+            {
+                if (source != value)
+                {
+                    source = value;
+                    OnPropertyChanged(nameof(Source));
+                }
+            }
+        }
+        public ICommand Fill => new Command(Request);
+
+
+        public async void Request()
+        {
+            client = new HttpClient();
+            var result = await client.GetStringAsync("https://reqres.in/api/users?page=2");
+            var users = JsonConvert.DeserializeObject<MyResponce>(result);
+            data = new ObservableCollection<Users>(users.Responce);
+            Source = data;
+        }
+    }
+}
